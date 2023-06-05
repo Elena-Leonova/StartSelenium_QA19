f152a36d8aaf04f85226af217f0688a19a2d52b7
Elena-Leonova <lena.postrash@gmail.com>
2023-06-05 19:03:20 +0300

    Update StartTest.java

--- src/test/java/StartTest.java
+++ src/test/java/StartTest.java
@@ -1,9 +1,12 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class StartTest{
     WebDriver wd;
@@ -22,6 +25,12 @@ public class StartTest{
         wd.findElement(By.id("root"));
         wd.findElement(By.linkText("HOME"));
         wd.findElement(By.partialLinkText("HO"));
+
+        WebElement element = wd.findElement(By.id("root"));
+        List<WebElement> list = wd.findElements(By.tagName("div"));
+        System.out.println(list.size());
+
+
     }
 
     @AfterTest

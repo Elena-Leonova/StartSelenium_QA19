47a607eb7a941ab3e09ef9323577d7c4e3fa5344
Elena-Leonova <lena.postrash@gmail.com>
2023-06-05 18:52:35 +0300

    Update StartTest.java

--- src/test/java/StartTest.java
+++ src/test/java/StartTest.java
@@ -1,27 +1,32 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class StartTest {
+public class StartTest{
     WebDriver wd;
 
     @BeforeTest
     public void precondition(){
         wd = new ChromeDriver();
         wd.get("https://telranedu.web.app/home");
-//navigate to the link
+
+
     }
 
     @Test
     public void testMethod(){
-//test login
+        wd.findElement(By.tagName("a"));
+        wd.findElement(By.id("root"));
+        wd.findElement(By.linkText("HOME"));
+        wd.findElement(By.partialLinkText("HO"));
     }
 
     @AfterTest
     public void postcondition(){
-//click logout
+        wd.quit();
     }
 
 }
